703166ca0e6b65ab549f5ea5f4792f9e94a7fdc9
SUTHARSHANA GAYATHRI US <125731789+sutharshana-gayathri@users.noreply.github.com>
2023-08-04 11:04:49 +0530

    Add files via upload

---
+++ jen.java
@@ -0,0 +1,14 @@
+package IncludeHelp;
+
+public class Print_1_To_10_UsingFor 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Output is : ");
+
+		for(int i = 1; i <= 10; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
