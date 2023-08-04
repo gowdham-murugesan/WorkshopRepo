a47e9f1c51b27f8ae0fd3a80cc259e0e262a108f
SUTHARSHANA GAYATHRI US <125731789+sutharshana-gayathri@users.noreply.github.com>
2023-08-04 11:21:03 +0530

    Add files via upload

---
+++ numbers.java
@@ -0,0 +1,15 @@
+package jen1;
+
+public class numbers {
+		public static void main(String[] args)
+		{
+			System.out.println("Output is : ");
+
+			for(int i = 1; i <= 10; i++)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
+
